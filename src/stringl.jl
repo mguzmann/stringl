ec068bf7d3aef21f2c0e9a5caa7c0e01e1f7c510
mortem.dei@gmail.com <mortem.dei@gmail.com>
2018-01-16 16:57:22 +0100

    Main module

---
+++ src/stringl.jl
@@ -0,0 +1,8 @@
+module Stringl
+
+export str_to_upper, str_to_lower, str_to_title, str_to_fupperend, str_to_flower, str_replace, str_count, str_length, str_detect, str_c, str_replace, str_replace_all
+
+include("string-functions.jl")
+include("additional-functions.jl")
+
+end
